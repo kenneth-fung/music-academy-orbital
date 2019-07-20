f0a9257c65071c8e6c3fe5b933dccdb40c9c7ad6
Kenneth Fung <fungchenyu@rocketmail.com>
2019-07-20 21:41:42 +0800

    K: Finish adding autocomplete to Tutors page search form

--- app/assets/javascripts/tutors.js
+++ app/assets/javascripts/tutors.js
@@ -1,23 +1,17 @@
 document.addEventListener("turbolinks:load", function() {
-  $input = $("[data-behavior='autocomplete']");
+  $input = $("[data-behavior='autocomplete-tutors']");
 
   var controller = $('.form-control').data('controller');
   var action     = $('.form-control').data('action');
 
   var options = {
-    getValue: "title",
+    getValue: "name",
     url: function(phrase) {
 
       if (controller == "tutors") {
-        if (action == "show") {
-          id = $('.form-control').data('id');
-          return "/tutors/" + id + "/courses.json?search_profile=" + phrase;
-        } 
-      } else if (controller == "students") {
-        id = $('.form-control').data('id');
-        return "/students/" + id + "/courses.json?search_profile=" + phrase;
-      } else {
-        return "/courses.json?search=" + phrase; 
+        if (action == "index") {
+          return "/tutors.json?search=" + phrase;
+        }
       }
 
     },
