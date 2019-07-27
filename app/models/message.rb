0bc36e9b16ce5aa801b1d6d8f4ba629e6f5d8dc8
Kenneth Fung <fungchenyu@rocketmail.com>
2019-07-27 16:54:43 +0800

    K: Fix posts and messages creation being rolled back due to validations

--- app/models/message.rb
+++ app/models/message.rb
@@ -8,13 +8,6 @@ class Message < ApplicationRecord
     presence: true,
     numericality: { greater_than_or_equal_to: 1 }
 
-  validates :user_type,
-    presence: true
-
-  validates :user_id,
-    presence: true,
-    numericality: { greater_than_or_equal_to: 1 }
-
   # Returns the user who sent the message
   def sender
     self.user_type.constantize.find(self.user_id)
--- app/models/post.rb
+++ app/models/post.rb
@@ -9,13 +9,6 @@ class Post < ApplicationRecord
     presence: true,
     numericality: { greater_than_or_equal_to: 1 }
 
-  validates :user_type,
-    presence: true
-
-  validates :user_id,
-    presence: true,
-    numericality: { greater_than_or_equal_to: 1 }
-
   # Returns the user who sent the post
   def sender
     self.user_type.constantize.find(self.user_id)
